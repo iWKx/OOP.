e315185d51c2f1f8f2a5b9988ff015200e3d387c
Wael Kattan <waelk@uia.no>
2023-09-19 22:10:31 +0200

    Sully_Special'

---
+++ sully.py
@@ -0,0 +1,42 @@
+import requests
+
+
+class Pet:
+    def __init__(self, name):
+        self.name = name
+
+    def speak(self):
+        pass  
+
+class Human(Pet):
+    def __init__(self, name, age):
+        super().__init__(name)
+        self.age = age
+
+    def speak(self):
+        print(f"{self.name} says: AlsalamuAleikum!")
+
+class Dog(Pet):
+    def __init__(self, name, breed):
+        super().__init__(name)
+        self.breed = breed
+
+    def speak(self):
+        print(f"{self.name} says: Woof! Woof!")
+
+class Cat(Pet):
+    def __init__(self, name, color):
+        super().__init__(name)
+        self.color = color
+
+    def speak(self):
+        print(f"{self.name} says: Meow Meow Nigga!")
+
+wael = Human("Wael", 23)
+Sully = Dog("Suleyman", "Turkish Pitbull")
+silver = Cat("Silver", "Sølv")
+
+# Using object methods
+wael.speak()
+Sully.speak()
+silver.speak()
